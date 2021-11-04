a6c935e63648028e2c8451fcee5fe83a0f492d46
oleg086rus <o.pototskiy@yandex.ru>
2021-11-05 00:10:05 +0500

    say hello

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//приветствие
 Route::get('/name/{name}', function(string $name) {
     return "Hello, {$name}";
 });
